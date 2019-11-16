7b98315db23c3ee4e59bc9eb1a1bbffa9d7680fe
angel m <angelumartinez9@gmail.com>
2019-11-16 08:46:57 -0500

    heroku

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 
 
 const db = require("./config/keys").mongoURI;
-mongoose.connect("mongodb://localhost/gamedb", { useNewUrlParser: true }).then(() => console.log("MongoDB connected")).catch((err) => console.log(err));
+mongoose.connect("mongodb://user:Milan10@ds031657.mlab.com:31657/heroku_1ctsgw6q", { useNewUrlParser: true }).then(() => console.log("MongoDB connected")).catch((err) => console.log(err));
 // mongoose.connect("mongodb://localhost/gamedb", { useNewUrlParser: true });
 
 
@@ -27,6 +27,14 @@ app.use(passport.initialize());
 require('./config/passport')(passport);
 
 app.use('/api/users', users);
+
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${3000}`);
