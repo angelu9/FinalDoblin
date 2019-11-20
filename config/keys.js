9c1fa4e945f38b9bb3549f2de01e2d7e716abffb
Hoi So <miss.hoiso@gmail.com>
2019-11-20 16:39:36 -0500

    heroku changes

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://localhost:27017/mern-passport",
+    mongoURI: 'mongodb://<ahoyhoi>:<ahoyhoi1>@ds119765.mlab.com:19765/heroku_ztvb2l6g' || "mongodb://localhost:27017/mern-passport",
     cypher: "blahblah"
 };
